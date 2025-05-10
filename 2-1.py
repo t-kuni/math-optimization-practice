6a215b37f536c66916f16c24da837bf61ec9be40
t-kuni <t.kuni.dev@gmail.com>
2025-05-11 00:06:08 +0900

    update

--- 2-1.py
+++ 2-1.py
@@ -16,3 +16,13 @@ print(pulp.LpStatus[result])
 pulp.value(p.objective)
 for v in p.variables():
     print(f'{v} = {pulp.value(v)}')
+
+print('\n---- 感度分析 ----\n')
+
+# 決定変数の値と限界コスト
+for v in p.variables():
+    print(f'{v} = {pulp.value(v):10}, Reduced cost = {v.dj}')
+
+# 潜在価格と余裕
+for cname, c in list(p.constraints.items()):
+    print(f'{cname:6s}, Shadow price = {c.pi:6}, Slack = {c.slack}')
